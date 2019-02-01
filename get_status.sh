53e983c900e0fc72bd405085b8f26b40687532b5
felixfaisal <faisalahmedfarooq46@gmail.com>
2019-02-02 05:24:24 +0530

    Sending the output to the text file, Succesfully test

--- get_status.sh
+++ get_status.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Prints a list of last logged in users, system uptime, current date/time
 
-printf "Last logged in users:"
-last -a
-printf "\n"
+printf "Last logged in users:" > status.txt
+last -a >> status.txt
+printf "\n" >> status.txt
 
-printf "System Uptime:"
-uptime
+printf "System Uptime:" >> status.txt
+uptime >> status.txt
 
-printf "\nDate:\n"
-date
+printf "\nDate:\n" >> status.txt
+date >> status.txt
 
 exit
